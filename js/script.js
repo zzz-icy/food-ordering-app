a9a574e8f3743df378420b52eef81a56e91d0551
Hannah Z <eveline1129@163.com>
2024-03-11 00:16:27 -0500

    implemented smooth scrolling on all browsers on tapping buttons

--- js/script.js
+++ js/script.js
@@ -27,3 +27,24 @@ const headerEl = document.querySelector(".header")
 btnNavEl.addEventListener("click", () => {
 	headerEl.classList.toggle("nav-open")
 })
+
+///////////////////////////////
+// SMooth scrolling animation
+///////////////////////////////
+// only selecting anchor elements with href set
+
+const allLinks = document.querySelectorAll("a:link")
+allLinks.forEach((link) => {
+	link.addEventListener("click", (e) => {
+		e.preventDefault()
+		const href = link.getAttribute("href")
+		// scroll back to top
+		if (href === "#") {
+			window.scrollTo({ top: 0, behavior: "smooth" })
+		}
+		if (href !== "#" && href.startsWith("#")) {
+			const sectionEl = document.querySelector(href)
+			sectionEl.scrollIntoView({ behavior: "smooth" })
+		}
+	})
+})
